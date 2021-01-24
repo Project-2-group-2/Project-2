990fbd77693cc0076db6e5229c498ee72cf511e1
Jacob Bradshaw <jakebradshaw123@gmail.com>
2021-01-24 13:39:24 -0800

    ties in pages, fixes jquery render, adds handlebars to second page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,17 @@
 module.exports = function(app) {
 
-    app.get("/", function(req, res) {
-          res.render("index");
-        });
-    
-    
-    
+  app.get("/", function(req, res) {
+        res.render("index");
+      });
+  
+  
+  
 
 
-    app.get("/parties", function(req, res) {
-      res.render("party");
-    });
+  app.get("/parties/:id", function(req, res) {
+    var event = {event: req.params.id}
+    res.render("party", event);
+  });
 
 
 };
